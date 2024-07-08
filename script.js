6f64cda667cf02b6e6ba67a3539bf44825d90b92
SowIbra24 <ibrahima.sow0608@gmail.com>
2024-07-08 14:37:49 +0200

    gestion de l'envoi du mail de contact avec emailjs

--- script.js
+++ script.js
@@ -35,4 +35,40 @@ document.addEventListener('DOMContentLoaded', function () {
         const isOpen = afficheMenu.classList.contains('open');
         menuBurgerIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
     });
+
+    //Envoi de l'email via le formulaire
+    const form = document.getElementById('contact-form');
+    const btn = document.querySelector('.cta');
+
+    emailjs.init("n_HiSOMK7N_KjBU9E");
+
+    function sendMail(event){
+        event.preventDefault();
+
+        const params = {
+            nom : document.querySelector('#nom').value,
+            mail : document.querySelector('#email').value,
+            message : document.querySelector('#message').value
+        }
+
+        const serviceID = 'default_service';
+        const templateID = 'template_i55jpqr';
+
+        btn.value = 'Envoi...';
+        btn.disabled = true;
+
+        emailjs.send(serviceID, templateID, params)
+            .then(() => {
+                btn.value = 'Envoyer';
+                btn.disabled = false;
+                alert('Votre message a été envoyé avec succès');
+                form.reset(); // Réinitialiser le formulaire après envoi
+            }, (err) => {
+                btn.value = 'Envoyer';
+                btn.disabled = false;
+                alert('Echec à l\'envoi.\n' + JSON.stringify(err));
+            });
+
+    }
+    form.addEventListener('submit', sendMail);
 });
