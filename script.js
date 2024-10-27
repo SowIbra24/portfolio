eb6eb4408d3f438f8e4d2566d02574659e51891e
SowIbra24 <ibrahima.sow0608@gmail.com>
2024-10-27 14:15:23 +0100

    corrections de la navigation sous mobile, ajout de texte dynamique, l'api pour recuperer les repos git

--- script.js
+++ script.js
@@ -5,36 +5,48 @@ document.addEventListener('DOMContentLoaded', function () {
     links.forEach(link => {
         link.addEventListener('click', function () {
             // Supprime la classe 'selected' de tous les liens
-            links.forEach(link => link.classList.remove('selected'));
-            
+            links.forEach(link => link.classList.remove('selected'));  
             // Ajoute la classe 'selected' au lien cliqué
             this.classList.add('selected');
         });
-    });
-
-    // Gestion du défilement de la page
-    const navigation = document.querySelector('.navigation');
-
-    window.addEventListener('scroll', function () {
-        if (window.scrollY > 0) {
-            navigation.classList.add('scrolled');
-            afficheMenu.classList.remove('open');
-            menuBurgerIcon.classList = 'fa-solid fa-bars';
-        } else {
-            navigation.classList.remove('scrolled');
-        }
     });
 
     // Gestion du menu burger
     const menuBurgerButton = document.querySelector('.menu-burger');
     const menuBurgerIcon = document.querySelector('.menu-burger i');
     const afficheMenu = document.querySelector('.nav-menu');
+    let isOpen =false;
 
-    menuBurgerButton.addEventListener('click', function () {
+    function toogleMenu()
+    {
         afficheMenu.classList.toggle('open');
-        const isOpen = afficheMenu.classList.contains('open');
+        isOpen = afficheMenu.classList.contains('open');
         menuBurgerIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+    }
+    menuBurgerButton.addEventListener('click', toogleMenu);
+
+    // Gestion de la fermeture du menu lors du clic sur un lien
+    const navLinks = document.querySelectorAll('.nav-lien');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            if (isOpen) {
+                toogleMenu();
+            }
+        });
     });
+    
+
+     // Gestion du défilement de la page
+     const navigation = document.querySelector('.navigation');
+
+     window.addEventListener('scroll', function () {
+         if (window.scrollY > 0) {
+             navigation.classList.add('scrolled');
+         } else {
+             navigation.classList.remove('scrolled');
+         }
+     });
 
     //Envoi de l'email via le formulaire
     const form = document.getElementById('contact-form');
@@ -71,4 +83,79 @@ document.addEventListener('DOMContentLoaded', function () {
 
     }
     form.addEventListener('submit', sendMail);
+
+    // projets github
+    let projects = document.getElementById("projet-card");
+        
+    function getData() {
+    return fetch('https://api.github.com/users/sowibra24/repos')
+    .then(response => response.json()) 
+    .catch(error => {
+    console.error('Erreur lors de la récupération des données:', error);
+    return null; 
+    });
+    }
+    getData().then(repos => {
+        repos
+            .filter(repo => repo["name"] !== "SowIbra24" && repo["name"] !== "portfolio" && repo["name"] !== "calculatrice_php")
+            .forEach(repo => {
+                const name = repo["name"];
+                const description = repo["description"];
+                const url = repo["svn_url"];
+                appendProjects(projects, name, description,url);
+            });
+    });
+
+    function appendProjects(projects, name, description,url) {
+
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.style.width = '18rem';
+        
+        // pour l'image
+        const img = document.createElement('img');
+        img.src = 'image/cards/'+name+'.png'; 
+        img.className = 'card-img-top';
+        img.alt = 'Une petite image qui illustre'+name;
+
+        // la carte bootstrap
+        const cardBody = document.createElement('div');
+        cardBody.className = 'card-body';
+
+        const cardTitle = document.createElement('h5');
+        cardTitle.className = 'card-title';
+        cardTitle.innerHTML = name; 
+
+        const cardText = document.createElement('p');
+        cardText.className = 'card-text';
+        cardText.innerHTML = description;
+
+        const button = document.createElement('a');
+        button.target = '_blank'
+        button.href = url; 
+        button.className = 'btn btn-primary';
+        button.innerHTML = 'Aller au repo git';
+
+        cardBody.appendChild(cardTitle);
+        cardBody.appendChild(cardText);
+        cardBody.appendChild(button);
+        card.appendChild(img);
+        card.appendChild(cardBody);
+
+        projects.appendChild(card);
+    }
+
+    // Ecrire de façon dynamique 
+    const typed = new Typed('.texte-js', {
+        strings : [
+            'un developpeur amateur 💻',
+            'intéressé par la cybersécurité ',
+            'étudiant en Systèmes Réseaux et Sécurité'
+        ],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
 });
+
